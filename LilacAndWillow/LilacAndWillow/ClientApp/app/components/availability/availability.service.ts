5f541c2f40f4465faedd3b4848566c95ec5df5f0
Mike Campbell <swimmerboi93@hotmail.com>
2018-10-18 18:01:14 -0600

    setting up google calendar api call.

---
+++ LilacAndWillow/LilacAndWillow/ClientApp/app/components/availability/availability.service.ts
@@ -0,0 +1,13 @@
+﻿import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators'
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AvailabilityService {
+
+    constructor(private http: HttpClient)
+}
